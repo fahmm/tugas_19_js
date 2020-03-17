6cb7c657d25fd70af050695ceef1fc4cbae64147
Fahmi <pammimajid@gmail.com>
2020-03-17 20:58:14 +0800

    ok

---
+++ tugas_19_js.js
@@ -0,0 +1,14 @@
+console.log("Semangat...".repeat(5));
+
+console.log("----------------------------")
+
+var a ="*****"
+console.log(a.repeat(1));
+console.log(a.repeat(5));
+console.log(a.repeat(9));
+console.log(a.repeat(13));
+console.log(a.repeat(19));
+console.log(a.repeat(13));
+console.log(a.repeat(9));
+console.log(a.repeat(5));
+console.log(a.repeat(1));
